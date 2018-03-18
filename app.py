b3fccc227b059a7b1c27bacd194e22ae6f3a8aae
mikbhub <mikolaj.babiak@gmail.com>
2018-03-18 15:27:51 +0100

    Moved to async request

--- app.py
+++ app.py
@@ -4,23 +4,40 @@ Api endpoint: /api/{body_part}/{location}
 location => default=warszawa
 body_part => head, limb, chest, back
 """
-from flask import Flask, make_response
-from flask_restful import Resource, Api
-from znany_data_source import get_medical_institutions
+import asyncio
 from functools import wraps
+
+from flask import Flask, make_response
+from flask_restful import Api, Resource
+
 from symptoms_map import MAPPER
+from znany_data_source import get_medical_institutions
+
+
+async def get_instituion(speciality, location):
+    return get_medical_institutions(speciality, location)
+
 
 app = Flask(__name__)
 api = Api(app)
 
 default_location = 'warszawa'
 
+
 class Data(Resource):
     def get(self, body_part, location=None):
         location = location or default_location
-        institutions = []
-        for speciality in MAPPER[body_part]:
-            institutions.extend(get_medical_institutions(speciality, location))
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        institutions = loop.run_until_complete(
+            asyncio.gather(
+                *list(
+                    get_instituion(speciality, location)
+                    for speciality, location in zip(MAPPER[body_part], (location for _ in MAPPER[body_part]))
+                )
+            )
+        )
+        loop.close()
         return institutions, {'Access-Control-Allow-Origin': '*'}
 
 
